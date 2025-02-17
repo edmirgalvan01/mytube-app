f93d53fcb9bde891e3f9d60f250fe7a07607072e
Edmir Galvan <edmirgalvaz@gmail.com>
2025-02-17 07:03:37 -0600

    Add logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <p className="font-bold text-rose-500">Hello World!</p>
-      <Button>Click me!</Button>
+      <Image src="/logo.svg" alt="Logo" height={50} width={50} />
+      <p className="text-xl font-smebildo tracking-tight">NewTube</p>
     </div>
   );
 }
