10d92ed7c5fd31f65d984349996903cc803d89de
Edmir Galvan <edmirgalvaz@gmail.com>
2025-02-20 07:13:32 -0600

    Personal section created

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div>
-      <Image src="/logo.svg" alt="Logo" height={50} width={50} />
-      <p className="text-xl font-smebildo tracking-tight">NewTube</p>
-    </div>
-  );
+  return <div>I will load videos in the future</div>;
 }
